a88abd420bc4047d7769eaabb1d3bc3a7239a201
Eliassg <46458446+Eliassg@users.noreply.github.com>
2024-04-29 22:29:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-# Load data from Excel file
-@st.cache_data
-def load_data():
-    df = pd.read_excel('data.xlsx')  # Update the file path accordingly
-    return df
+# Hardcoded data as a DataFrame
+data = pd.DataFrame({
+    'Uke': [1, 2, 3, 4, 5, 6, 7],
+    'Reisekassa': [600, 600, 600, 600, 600, 1411, 1769],
+    'Baseline': [800, 1000, 1200, 1400, 1600, 2200, 2400]
+})
 
 # Function to create line chart
 def line_chart(data):
@@ -17,7 +18,7 @@ def line_chart(data):
     chart = alt.Chart(melted_data).mark_line().encode(
         x=alt.X('Uke:Q', axis=alt.Axis(format='d')),  # Display integer increments on x-axis
         y=alt.Y('Value:Q', axis=alt.Axis(title='NOK')),  # Shared y-axis title
-        color=alt.Color('Category:N', legend=alt.Legend(title=' '), scale=alt.Scale(domain=['Reisekassa', 'Baseline'], range=['blue', 'darkred'])),  # Separate lines by category and set custom colors
+        color=alt.Color('Category:N', legend=alt.Legend(), scale=alt.Scale(domain=['Reisekassa', 'Baseline'], range=['blue', 'darkred'])),  # Separate lines by category and set custom colors
         tooltip=['Uke', 'Value']
     ).properties(
         width=700,
@@ -33,9 +34,6 @@ def line_chart(data):
 # Main function to run the Streamlit app
 def main():
     st.set_page_config(page_title='Tippelaget Analytics', layout='wide')
-
-    # Load data
-    data = load_data()
 
     # Custom theme
     st.altair_chart(line_chart(data), use_container_width=True)
